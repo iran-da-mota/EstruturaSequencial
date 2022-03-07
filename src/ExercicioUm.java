3d226d684bc5ded162f438edd72509df5eca0858
iran-da-mota <iran.dev.ic3ga@gmail.com>
2022-03-06 22:45:02 -0300

    Initial commit

---
+++ src/ExercicioUm.java
@@ -0,0 +1,9 @@
+/*
+Estrutura Sequencial
+Faça um Programa que mostre a mensagem "Olá mundo" na tela.
+ */
+public class ExercicioUm {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+    }
+}
