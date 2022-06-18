c92be7bd9c72b9cc1e2f272d63075cebde319d58
Iran da Mota <88514462+iran-da-mota@users.noreply.github.com>
2022-06-18 18:43:37 -0300

    Delete ExercicioUm.java

--- src/ExercicioUm.java
+++
@@ -1,9 +0,0 @@
-/*
-Estrutura Sequencial
-Faça um Programa que mostre a mensagem "Olá mundo" na tela.
- */
-public class Exercicio01 {
-    public static void main(String[] args) {
-        System.out.println("Olá mundo!");
-    }
-}
