530e06d74c3cc50ba5ffca9b1a09072b574a972e
Iran da Mota <88514462+iran-da-mota@users.noreply.github.com>
2022-06-18 18:38:13 -0300

    Update ExercicioUm.java

--- src/ExercicioUm.java
+++ src/ExercicioUm.java
@@ -2,7 +2,7 @@
 Estrutura Sequencial
 Faça um Programa que mostre a mensagem "Olá mundo" na tela.
  */
-public class ExercicioUm {
+public class Exercicio01 {
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
     }
